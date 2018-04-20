a89e23a5294484a0c0921c99b2ac11fa8fd3044f
Raul <raul@frifee.co>
2018-04-20 20:15:48 +0900

    modified:   steph_admin/static/admin/js/swips.js 	new file:   steph_admin/templates/admin/static/admin/css/base.css 	new file:   steph_admin/templates/admin/static/admin/css/changelists.css 	new file:   steph_admin/templates/admin/static/admin/css/custom_list.css 	new file:   steph_admin/templates/admin/static/admin/css/modal_custom.css 	new file:   steph_admin/templates/admin/static/admin/js/curry_news.js 	new file:   steph_admin/templates/admin/static/admin/js/curry_vods.js 	new file:   steph_admin/templates/admin/static/admin/js/input_tag.js 	new file:   steph_admin/templates/admin/static/admin/js/lineup.js 	modified:   steph_admin/templates/admin/steph_admin/change_list_custom.html 	modified:   steph_admin/templates/admin/steph_admin/change_list_lineup.html 	modified:   steph_admin/templates/admin/steph_admin/change_list_news.html 	modified:   steph_admin/templates/admin/steph_admin/menu.html 	new file:   steph_admin/templates/admin/templates/admin/base_site.html 	new file:   steph_admin/templates/admin/templates/admin/change_form.html 	new file:   steph_admin/templates/admin/templates/admin/change_list.html 	new file:   steph_admin/templates/admin/templates/admin/delete_confirmation.html 	new file:   steph_admin/templates/admin/templates/admin/index.html 	new file:   steph_admin/templates/admin/templates/admin/steph_admin/change_list_custom.html 	copied:     steph_admin/templates/admin/steph_admin/change_list_news.html -> steph_admin/templates/admin/templates/admin/steph_admin/change_list_news.html 	new file:   steph_admin/templates/admin/templates/admin/steph_admin/change_list_vods.html

--- steph_admin/static/admin/js/swips.js
+++ steph_admin/static/admin/js/swips.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
+  var clone_menu = $('#swips_menu').clone();
+  clone_menu.appendTo('#grp-navigation');
 
 });
+
+$('#grp-content').ready(function(){
+  $('#grp-content').css({top: 0});
+});
+$('#grp-header').ready(function(){
+  $('#grp-header').css({'position':'relative'});
+});
+// $('#grp-content').attrchange({
+//     trackValues: true, /* Default to false, if set to true the event object is
+//                 updated with old and new value.*/
+//     callback: function (event) {
+//       // $('#grp-navigation').attr('position','relative');
+//       $('#grp-content').attr('top','0px');
+//       // console.log('go');
+//       // console.log($('#grp-navigation').attr('position'));
+//       // console.log($('#grp-content').attr('position'));
+//     }
+// });
