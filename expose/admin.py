6cc79774b430eb557c687a32f4c472081fd47752
Raul <raul@frifee.co>
2018-04-25 14:47:42 +0900

    modified:   development/admin.py 	modified:   etc/admin.py 	modified:   expose/admin.py 	modified:   feedback/admin.py 	modified:   feeds/admin.py

--- expose/admin.py
+++ expose/admin.py
@@ -5,8 +5,10 @@ from .models import *
 @admin.register(CurryExposePostLog)
 class CurryExposePostLog(admin.ModelAdmin):
     list_display = ('id', 'desc', 'user', 'ut', 'target')
+    search_fields = ('id', 'desc', 'user', 'ut', 'target')
     change_list_template = 'admin/steph_admin/change_list_custom.html'
 @admin.register(CurryExposeTarget)
 class CurryExposeTargetAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'desc', 'ut')
+    search_fields = ('id', 'name', 'desc', 'ut')
     change_list_template = 'admin/steph_admin/change_list_custom.html'
