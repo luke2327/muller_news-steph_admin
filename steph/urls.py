4ef7c89efd9f54b9d2e146121b174aa02b422eea
Raul <raul@frifee.co>
2018-05-02 14:19:50 +0900

    modified:   steph/urls.py

--- steph/urls.py
+++ steph/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 urlpatterns = [
     path('grappelli/', include('grappelli.urls')), # grappelli URLS
-    path('admin_v2/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('steph_admin/', include('steph_admin.urls')),
 ]
