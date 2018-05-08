9114ffe51f3e0c82fbe9664c1f3f16c311d42591
Raul <raul@frifee.co>
2018-05-08 11:12:59 +0900

    modified:   league_team_player/admin.py 	modified:   transfer/models.py

--- league_team_player/admin.py
+++ league_team_player/admin.py
@@ -103,8 +103,11 @@ class SwipsPlayerCareerAdmin(admin.ModelAdmin):
 class CurryPlayerAdmin(admin.ModelAdmin):
     list_display = ('player', 'name', 'mid_name', 'name_ko', 'mid_name_ko', 'name_th',
                     'mid_name_th', 'country', 'social', 'draft', 'school', 'ut', 'position')
+    search_fields = ['player', 'name', 'mid_name', 'name_ko', 'mid_name_ko', 'name_th',
+                    'mid_name_th', 'country', 'social', 'draft', 'school', 'ut', 'position']
     def has_add_permission(self, request):
         return False
+
 admin.site.register(SwipsPlayerInfo,SwipsPlayerInfoAdmin)
 admin.site.register(SwipsTeamInfo,SwipsTeamInfoAdmin)
 admin.site.register(SwipsLeagueInfo,SwipsLeagueInfoAdmin)
